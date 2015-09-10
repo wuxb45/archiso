294b2ceeccb94f40110e78bad08bc976215380c1
Wu Xingbo <wuxb45@gmail.com>
2015-09-10 14:21:41 -0400

    remove i686, delete MyConfig

--- configs/releng/build_wuxb.sh
+++ configs/releng/build_wuxb.sh
@@ -1,4 +1,6 @@
 # wuxb: install skel
 mkdir -p ${work_dir}/${arch}/airootfs/etc/skel
+rm -rf ${work_dir}/MyConfig
 git clone https://github.com/wuxb45/MyConfig.git ${work_dir}/MyConfig
 ${work_dir}/MyConfig/install.sh ${work_dir}/${arch}/airootfs/etc/skel ${work_dir}/MyConfig
+rm -rf ${work_dir}/MyConfig
